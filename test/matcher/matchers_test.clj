7397c4631e242248929b5e6dd79f0ca40a455129
Maurício Szabo <mauricio.szabo@gmail.com>
2016-10-07 11:12:53 -0300

    Macro creation, and code cleanup

--- test/matcher/matchers_test.clj
+++ test/matcher/matchers_test.clj
@@ -3,32 +3,27 @@
             [matcher.matchers :as m]))
 
 (def test-list (list 1 2 3))
-(subvec (vec test-list) 0 1)
 (fact "Matching against lists"
   ((m/list 1 2 3) [1 2 3]) => nil
   ((m/list 1 2 3) test-list) => [[1 2 3] [1 2 3]]
   ((m/list 1 '?foo 3) test-list) => [[1 '?foo 3] [1 2 3]]
   ((m/list 1 '& '?rest) test-list) => [[1 '?rest] [1 '(2 3)]])
 
-(facts "about match*"
-  (background
-    (gensym) => 'foo)
+(facts "Using match macro"
+  (fact "matching without vars"
+    (m/match test-list
+      (m/list 1 2 1) :foo
+      (m/list 1 2 3) :bar) => :bar
 
-  (fact ""))
+    (m/match '(1 2 (2 3) 4)
+      (m/list 1 2 '(2 2) 4) :foo
+      (m/list 1 2 (m/list 2 3) 4) :bar) => :bar)
 
-  ; (fact "combines results and matches then"
-  ;   (macroexpand-1 '(m/match test-list
-  ;                     (m/list 1 2 3) :foo))
-  ;   => `(if-let [~'foo (~'m/list ~'test-list 1 2 3)]
-  ;         :foo
-  ;         (throw (IllegalArgumentException. "No match")))))
+  (fact "matching with vars"
+    (m/match test-list
+      (m/list ?a 2 ?a) :foo
+      (m/list ?a 2 ?b) (+ a b)) => 4
 
-(fact "extracting values for pattern-matching"
-  (m/extract-vals test-list (m/list 1 2)) => nil
-  (m/extract-vals test-list (m/list 1 2 3)) => {:vals [[1 1] [2 2] [3 3]]}
-  (m/extract-vals test-list (m/list ?b ?a 3)) => {:bindings [['b 1]
-                                                             ['a 2]]
-                                                  :vals [[3 3]]}
-  (m/extract-vals test-list (m/list ?b ?a ?b)) => {:bindings [['b 1]
-                                                              ['a 2]]
-                                                   :vals [['b 3]]})
+    (m/match '(1 2 (2 3) 4)
+      (m/list ?a 2 (m/list ?a ?a) 4) :foo
+      (m/list 1 ?a (m/list ?a ?b) ?c) (+ a b c)) => 9))
