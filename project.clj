7dd1b9cdd9cd38a1fc94d3188f0390917bea7927
Maurício Szabo <mauricio.szabo@gmail.com>
2016-10-20 11:17:18 -0200

    Simple project.clj correction.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject matchbox "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject matchbox "0.1.0"
+  :description "Simple and extensive pattern-matching for Clojure"
   :url "https://github.com/mauricioszabo/matchbox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
